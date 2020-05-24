de83c4e26aa452a12109707f9003b4a90374bf3e
rohangupta4398 <rohangupta4398@gmail.com>
2020-05-24 17:33:58 +0530

    week2 prog assignment complete

---
+++ corr.R
@@ -0,0 +1,24 @@
+corr <- function(directory, threshold =0){
+  cor_results <- numeric(0)
+  
+  complete_cases <- complete(directory)
+  complete_cases <- complete_cases[complete_cases$nobs>=threshold, ]
+  #print(complete_cases["id"])
+  #print(unlist(complete_cases["id"]))
+  #print(complete_cases$id)
+  
+  if(nrow(complete_cases)>0){
+    for(monitor in complete_cases$id){
+      path <- paste(getwd(), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep = "")
+      #print(path)
+      monitor_data <- read.csv(path)
+      #print(monitor_data)
+      interested_data <- monitor_data[(!is.na(monitor_data$sulfate)), ]
+      interested_data <- interested_data[(!is.na(interested_data$nitrate)), ]
+      sulfate_data <- interested_data["sulfate"]
+      nitrate_data <- interested_data["nitrate"]
+      cor_results <- c(cor_results, cor(sulfate_data, nitrate_data))
+    }
+  }
+  cor_results
+}
